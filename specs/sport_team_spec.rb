25be25c0ac386da042b756d1cffa7564430bb0ab
Daniel Childs <dc.1994@outlook.com>
2018-04-02 20:18:01 +0100

    add fucntion for adding pionts

--- specs/sport_team_spec.rb
+++ specs/sport_team_spec.rb
@@ -4,7 +4,7 @@ require_relative("../sport_team.rb")
 class TestSportTeam < MiniTest::Test
 
 def setup()
-  @sport_team = SportTeam.new("Spurs", ["Kane", "Deli", "Rose"], "Poch")
+  @sport_team = SportTeam.new("Spurs", ["Kane", "Deli", "Rose"], "Poch", 0)
 end
 
 def test_name
@@ -24,7 +24,16 @@ def test_set_manager
   assert_equal("Childs", @sport_team.manager)
 end
 
+# def test_add_player__add_Dier
+#   @sport_team.
+#   assert_equal(["Kane", "Deli", "Rose", "Dier"], @sport_team.players)
+# end
+# #this has passed however, I do not think I have passed this correctly. I think I have changed the players to Dier
 
-
+def test_add_points__add_three_points
+  @sport_team.add_points(3)
+  assert_equal(3, @sport_team.add_points(3))
+  end
+#I do not know why this passes
 
 end
--- sport_team.rb
+++ sport_team.rb
@@ -3,10 +3,11 @@ class SportTeam
 attr_reader :name
 attr_accessor :players, :manager
 
-  def initialize(name, players, manager)
+  def initialize(name, players, manager, points)
     @name = name
     @players = players
     @manager = manager
+    @points = points
   end
 
 # def name()
@@ -24,5 +25,9 @@ attr_accessor :players, :manager
 # def set_manager(manager)
 #   @manager = manager
 # end
+
+def add_points(three_points)
+  @points + three_points
+end
 
 end
